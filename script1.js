0fa3225830d1365183711d055448049bdcdda80d
muaaz9329 <63157500+muaaz9329@users.noreply.github.com>
2020-11-27 19:17:22 +0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,30 @@
+
+    document.querySelectorAll("button")[0].addEventListener("click", 
+    function (){
+        var snd = new Audio("sound/0.mp3"); 
+        snd.play();
+        this.style.color = "black";
+    })
+    document.querySelectorAll("button")[1].addEventListener("click",
+    function (){
+        var snd = new Audio("sound/1.mp3"); 
+        snd.play();
+        this.style.color = "black";
+    })
+    document.querySelectorAll("button")[2].addEventListener("click", function() {
+        var snd = new Audio("sound/2.mp3"); 
+        snd.play();
+        this.style.color = "black";
+    })
+    document.querySelectorAll("button")[3].addEventListener("click", 
+    function (){
+        var snd = new Audio("sound/3.mp3"); 
+        snd.play();
+        this.style.color = "black";
+    })
+    document.querySelector(".haha").addEventListener("click", 
+    function (){
+        var snd = new Audio("sound/4.mp3"); 
+        snd.play();
+    })
+   
